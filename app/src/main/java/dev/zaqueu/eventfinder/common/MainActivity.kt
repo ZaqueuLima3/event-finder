37551a6267fb02b7d9a955e92943b52537ba911e
Zaqueu Santos <zaqueulima1@gmail.com>
2021-08-28 13:06:49 -0300

    feat: create app fragment factory

--- app/src/main/java/dev/zaqueu/eventfinder/common/MainActivity.kt
+++ app/src/main/java/dev/zaqueu/eventfinder/common/MainActivity.kt
@@ -3,10 +3,16 @@ package dev.zaqueu.eventfinder.common
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import dev.zaqueu.eventfinder.R
+import dev.zaqueu.eventfinder.common.presentation.AppFragmentFactory
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setupAppFragmentFactory()
         setContentView(R.layout.activity_main)
+    }
+
+    private fun setupAppFragmentFactory() {
+        supportFragmentManager.fragmentFactory = AppFragmentFactory()
     }
 }
---
+++ app/src/main/java/dev/zaqueu/eventfinder/common/presentation/AppFragmentFactory.kt
@@ -0,0 +1,13 @@
+package dev.zaqueu.eventfinder.common.presentation
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentFactory
+import org.koin.java.KoinJavaComponent.getKoin
+
+class AppFragmentFactory : FragmentFactory() {
+    override fun instantiate(classLoader: ClassLoader, className: String): Fragment {
+        val fragmentClass = Class.forName(className).kotlin
+        val instance = getKoin().getOrNull<Fragment>(fragmentClass)
+        return instance ?: super.instantiate(classLoader, className)
+    }
+}
