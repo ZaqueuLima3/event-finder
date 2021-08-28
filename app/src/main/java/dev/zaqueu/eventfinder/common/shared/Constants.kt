6cec97ab3e2c81c929837d448841c72a108e1487
Zaqueu Santos <zaqueulima1@gmail.com>
2021-08-28 13:09:07 -0300

    refactor: rename shared package to utils package

--- app/src/main/java/dev/zaqueu/eventfinder/common/shared/Constants.kt
+++
@@ -1,5 +0,0 @@
-package dev.zaqueu.eventfinder.common.shared
-
-object Constants {
-    const val SICREDI_BASE_URL = "http://5f5a8f24d44d640016169133.mockapi.io/api"
-}
---
+++ app/src/main/java/dev/zaqueu/eventfinder/common/utils/Constants.kt
@@ -0,0 +1,5 @@
+package dev.zaqueu.eventfinder.common.utils
+
+object Constants {
+    const val API_BASE_URL = "http://5f5a8f24d44d640016169133.mockapi.io/api/"
+}
