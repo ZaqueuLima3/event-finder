0ffd81582e25d2992a1980428e67930ce99652bf
Zaqueu Santos <zaqueulima1@gmail.com>
2021-08-27 21:30:31 -0300

    chore: create network module dependencies injection

---
+++ app/src/main/java/dev/zaqueu/eventfinder/common/data/di/NetworkModule.kt
@@ -0,0 +1,54 @@
+package dev.zaqueu.eventfinder.common.data.di
+
+import android.util.Log
+import com.google.gson.GsonBuilder
+import dev.zaqueu.eventfinder.common.data.remote.EventApi
+import dev.zaqueu.eventfinder.common.shared.Constants.SICREDI_BASE_URL
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import org.koin.core.module.Module
+import org.koin.dsl.module
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object NetworkModule {
+    val modules = networkModules()
+    private const val OK_HTTP = "okHttp"
+
+    private fun networkModules(): Module {
+        return module {
+            single {
+                val interceptor = HttpLoggingInterceptor {
+                    Log.d(OK_HTTP, it)
+                }
+                interceptor.level = HttpLoggingInterceptor.Level.BODY
+                OkHttpClient.Builder()
+                    .addInterceptor(interceptor)
+                    .build()
+            }
+            single {
+                GsonConverterFactory.create(GsonBuilder().create())
+            }
+            single {
+                createService<EventApi>(
+                    baseUrl = SICREDI_BASE_URL,
+                    client = get(),
+                    factory = get()
+                )
+            }
+        }
+    }
+
+    private inline fun <reified T> createService(
+        baseUrl: String,
+        client: OkHttpClient,
+        factory: GsonConverterFactory
+    ): T {
+        return Retrofit.Builder()
+            .baseUrl(baseUrl)
+            .client(client)
+            .addConverterFactory(factory)
+            .build()
+            .create(T::class.java)
+    }
+}
---
+++ app/src/main/java/dev/zaqueu/eventfinder/common/shared/Constants.kt
@@ -0,0 +1,5 @@
+package dev.zaqueu.eventfinder.common.shared
+
+object Constants {
+    const val SICREDI_BASE_URL = "http://5f5a8f24d44d640016169133.mockapi.io/api"
+}
