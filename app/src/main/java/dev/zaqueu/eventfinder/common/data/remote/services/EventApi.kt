30b5e96236adbcbf964ffe91012389bb31ac5851
Zaqueu Santos <zaqueulima1@gmail.com>
2021-08-28 13:08:27 -0300

    refactor: move EventApi to service package

--- app/src/main/java/dev/zaqueu/eventfinder/common/data/remote/services/EventApi.kt
+++ app/src/main/java/dev/zaqueu/eventfinder/common/data/remote/services/EventApi.kt
@@ -1,9 +1,9 @@
-package dev.zaqueu.eventfinder.common.data.remote
+package dev.zaqueu.eventfinder.common.data.remote.services
 
 import dev.zaqueu.eventfinder.common.data.remote.model.EventResponse
 import retrofit2.http.GET
 
 interface EventApi {
-    @GET("/events")
+    @GET("events")
     suspend fun getEvents(): List<EventResponse>
 }
