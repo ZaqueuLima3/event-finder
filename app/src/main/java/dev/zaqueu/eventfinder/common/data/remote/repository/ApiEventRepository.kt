8ee219a88fc897cd4b37ef5c5875b8e2bf1a95b2
Zaqueu Santos <zaqueulima1@gmail.com>
2021-08-28 13:07:43 -0300

    refactor: rename event repository implementation

--- app/src/main/java/dev/zaqueu/eventfinder/common/data/remote/repository/ApiEventRepository.kt
+++ app/src/main/java/dev/zaqueu/eventfinder/common/data/remote/repository/ApiEventRepository.kt
@@ -1,11 +1,11 @@
 package dev.zaqueu.eventfinder.common.data.remote.repository
 
-import dev.zaqueu.eventfinder.common.data.remote.EventApi
+import dev.zaqueu.eventfinder.common.data.remote.services.EventApi
 import dev.zaqueu.eventfinder.common.data.remote.mapper.mapToModel
 import dev.zaqueu.eventfinder.common.domain.model.Event
 import dev.zaqueu.eventfinder.common.domain.repository.EventRepository
 
-class SicrediEventRepository(
+class ApiEventRepository(
     private val eventApi: EventApi
 ) : EventRepository {
     override suspend fun getEvents(): List<Event> {
