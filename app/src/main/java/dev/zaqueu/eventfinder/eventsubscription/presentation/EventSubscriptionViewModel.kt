99789a996099314057780c0877982c88097e13bd
Zaqueu Santos <zaqueulima1@gmail.com>
2021-08-29 14:57:23 -0300

    feat: ensure checkIn viewModel method is correctly

--- app/src/main/java/dev/zaqueu/eventfinder/eventsubscription/presentation/EventSubscriptionViewModel.kt
+++ app/src/main/java/dev/zaqueu/eventfinder/eventsubscription/presentation/EventSubscriptionViewModel.kt
@@ -2,13 +2,27 @@ package dev.zaqueu.eventfinder.eventsubscription.presentation
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import dev.zaqueu.eventfinder.common.domain.model.CheckIn
+import dev.zaqueu.eventfinder.eventsubscription.domain.usecase.CheckInEventUseCase
+import kotlinx.coroutines.launch
 
-class EventSubscriptionViewModel : ViewModel() {
-    private val _isSucceed = MutableLiveData<Boolean>()
-    val isSucceed = _isSucceed
+class EventSubscriptionViewModel(
+    private val checkInEventUseCase: CheckInEventUseCase
+) : ViewModel() {
+    private val _checkInState = MutableLiveData<CheckInState>()
+    val checkInState = _checkInState
 
     fun checkInEvent(checkIn: CheckIn) {
-        _isSucceed.postValue(true)
+        viewModelScope.launch {
+            val result = checkInEventUseCase(checkIn)
+            result.onSuccess { _checkInState.postValue(CheckInState.Success) }
+            result.onFailure { _checkInState.postValue(CheckInState.Error) }
+        }
     }
+}
+
+sealed class CheckInState {
+    object Success : CheckInState()
+    object Error : CheckInState()
 }
---
+++ app/src/test/java/dev/zaqueu/eventfinder/eventsubscription/presentation/EventSubscriptionViewModelTest.kt
@@ -0,0 +1,78 @@
+package dev.zaqueu.eventfinder.eventsubscription.presentation
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import dev.zaqueu.eventfinder.TestCoroutineRule
+import dev.zaqueu.eventfinder.builders.CheckInTestBuilder
+import dev.zaqueu.eventfinder.eventsubscription.domain.usecase.CheckInEventUseCase
+import dev.zaqueu.eventfinder.utils.getOrAwaitValueTest
+import io.mockk.coEvery
+import io.mockk.coVerify
+import io.mockk.mockk
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.ObsoleteCoroutinesApi
+import kotlinx.coroutines.test.runBlockingTest
+import org.junit.Assert
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+
+@ExperimentalCoroutinesApi
+@ObsoleteCoroutinesApi
+class EventSubscriptionViewModelTest {
+    @get:Rule
+    val instantExecutorRule = InstantTaskExecutorRule()
+    @get:Rule
+    val testCoroutineRule = TestCoroutineRule()
+
+    private val checkInEventUseCase = mockk<CheckInEventUseCase>()
+    private lateinit var eventSubscriptionViewModel: EventSubscriptionViewModel
+
+    @Test
+    @Before
+    fun `test sut must not be null`() {
+        eventSubscriptionViewModel = EventSubscriptionViewModel(
+            checkInEventUseCase
+        )
+        Assert.assertNotNull(eventSubscriptionViewModel)
+    }
+
+    @Test
+    fun `should call checkInEventUseCase with correct values`() = runBlockingTest {
+        val checkIn = CheckInTestBuilder.build()
+        coEvery {
+            checkInEventUseCase(any())
+        } returns Result.success(Unit)
+        eventSubscriptionViewModel.checkInEvent(checkIn)
+        coVerify {
+            checkInEventUseCase(checkIn)
+        }
+    }
+
+    @Test
+    fun `should set a CheckInState as success if it succeed`() {
+        val checkIn = CheckInTestBuilder.build()
+        coEvery {
+            checkInEventUseCase(any())
+        } returns Result.success(Unit)
+        eventSubscriptionViewModel.checkInEvent(checkIn)
+        val viewState = eventSubscriptionViewModel.checkInState.getOrAwaitValueTest()
+        Assert.assertEquals(
+            CheckInState.Success,
+            viewState
+        )
+    }
+
+    @Test
+    fun `should set a CheckInState as error if it failure`() {
+        val checkIn = CheckInTestBuilder.build()
+        coEvery {
+            checkInEventUseCase(any())
+        } returns Result.failure(Exception())
+        eventSubscriptionViewModel.checkInEvent(checkIn)
+        val viewState = eventSubscriptionViewModel.checkInState.getOrAwaitValueTest()
+        Assert.assertEquals(
+            CheckInState.Error,
+            viewState
+        )
+    }
+}
