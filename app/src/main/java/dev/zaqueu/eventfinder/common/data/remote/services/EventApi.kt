0c9ba6b79f2edd4418c4620e741a21665816603f
Zaqueu Santos <zaqueulima1@gmail.com>
2021-08-29 13:44:10 -0300

    feat: create checkInEvent API

--- app/src/main/java/dev/zaqueu/eventfinder/common/data/remote/services/EventApi.kt
+++ app/src/main/java/dev/zaqueu/eventfinder/common/data/remote/services/EventApi.kt
@@ -1,9 +1,19 @@
 package dev.zaqueu.eventfinder.common.data.remote.services
 
 import dev.zaqueu.eventfinder.common.data.remote.model.EventResponse
+import dev.zaqueu.eventfinder.common.domain.model.CheckIn
+import retrofit2.http.Field
 import retrofit2.http.GET
+import retrofit2.http.POST
 
 interface EventApi {
     @GET("events")
     suspend fun getEvents(): List<EventResponse>
+
+    @POST
+    suspend fun checkInEvent(
+        @Field("eventId") eventId: String,
+        @Field("name") name: String,
+        @Field("email") email: String,
+    )
 }
