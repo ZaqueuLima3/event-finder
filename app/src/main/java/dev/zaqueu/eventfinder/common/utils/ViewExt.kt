b6b3f8839309cb9e1b65a22e6b562af799539bea
Zaqueu Santos <zaqueulima1@gmail.com>
2021-08-29 13:30:53 -0300

    feat: create view extention to hide keyboard

--- app/src/main/java/dev/zaqueu/eventfinder/common/utils/ViewExt.kt
+++ app/src/main/java/dev/zaqueu/eventfinder/common/utils/ViewExt.kt
@@ -1,6 +1,8 @@
 package dev.zaqueu.eventfinder.common.utils
 
+import android.content.Context
 import android.view.View
+import android.view.inputmethod.InputMethodManager
 
 fun View.handleSingleClick(debounceTime: Long = 500L, callback: () -> Unit) {
     if (this.isClickable) {
@@ -13,4 +15,9 @@ fun View.handleSingleClick(debounceTime: Long = 500L, callback: () -> Unit) {
             debounceTime
         )
     }
+}
+
+fun View.hideKeyboard() {
+    val imm = context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+    imm.hideSoftInputFromWindow(windowToken, 0)
 }
